cec8ab919fbe566a37ef49b671d40b95e28f07ff
ShadowPlasma2009 <shadowplasma2009@gmail.com>
2026-08-12 10:05:32 +0300

    Environment variables are behaving weirdly

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
 int main(void) {
-  //TODO
+  char *host = getenv("HOST");
+  char *user = getenv("USERNAME");
+
+  char cwd[4096];
+  getcwd(cwd, sizeof(cwd));
+  
+  // testing
+  printf("%s\n%s\n%s", host, user, cwd);
 }
